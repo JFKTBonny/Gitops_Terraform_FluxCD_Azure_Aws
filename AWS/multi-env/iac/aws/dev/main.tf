40f318bf79738f342572eef60c4ca24abbad76f6
JFKTBonny <jkamkotoyip@yahoo.com>
2025-08-20 17:12:15 +0200

    added  eks  modules in repo

--- AWS/multi-env/iac/aws/dev/main.tf
+++ AWS/multi-env/iac/aws/dev/main.tf
@@ -1,191 +1,26 @@
-
-
-
-
 provider "aws" {
-  region = "eu-central-2" # Switzerland (Zurich)
+  region  = var.region
+  profile = "default"
 }
 
-############################
-# Networking (VPC + Subnets)
-############################
+#create eni in the dataplane (step x)
+module "cluster" {
+  source = "../base/modules/eks"
+  #ENI subnet = private subnets
+  private_subnet_ids = module.vpc.private_subnet_ids
+  environment        = var.environment
 
-resource "aws_vpc" "gitops_vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-
-  tags = {
-    Name = "gitops-${var.environment}-vpc"
-  }
 }
 
-resource "aws_subnet" "default_subnet" {
-  vpc_id                  = aws_vpc.gitops_vpc.id
-  cidr_block              = "10.0.0.0/24"
-  availability_zone       = "eu-central-2a"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "default-subnet"
-  }
+module "vpc" {
+  source                    = "../base/modules/network"
+  cluster_security_group_id = module.cluster.cluster_security_group_id
+  environment               = var.environment
 }
+module "nodes" {
+  source       = "../base/modules/node-group"
+  cluster_name = module.cluster.cluster_name
+  subnet_ids   = module.vpc.private_subnet_ids
+  environment  = var.environment
 
-resource "aws_subnet" "eks_subnet" {
-  vpc_id                  = aws_vpc.gitops_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "eu-central-2b"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "gitops-${var.environment}-eks-subnet"
-  }
-}
-
-resource "aws_internet_gateway" "gitops_igw" {
-  vpc_id = aws_vpc.gitops_vpc.id
-
-  tags = {
-    Name = "gitops-${var.environment}-igw"
-  }
-}
-
-resource "aws_route_table" "gitops_rt" {
-  vpc_id = aws_vpc.gitops_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gitops_igw.id
-  }
-
-  tags = {
-    Name = "gitops-${var.environment}-rt"
-  }
-}
-
-resource "aws_route_table_association" "default_assoc" {
-  subnet_id      = aws_subnet.default_subnet.id
-  route_table_id = aws_route_table.gitops_rt.id
-}
-
-resource "aws_route_table_association" "eks_assoc" {
-  subnet_id      = aws_subnet.eks_subnet.id
-  route_table_id = aws_route_table.gitops_rt.id
-}
-
-############################
-# IAM Roles for EKS
-############################
-
-# Role for EKS control plane
-resource "aws_iam_role" "gitops_eks_role" {
-  name = "gitops-${var.environment}-eks-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect    = "Allow"
-      Principal = { Service = "eks.amazonaws.com" }
-      Action    = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "gitops_eks_cluster_policy" {
-  role       = aws_iam_role.gitops_eks_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "gitops_eks_vpc_policy" {
-  role       = aws_iam_role.gitops_eks_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-}
-
-# Role for EKS worker nodes
-resource "aws_iam_role" "gitops_node_role" {
-  name = "gitops-${var.environment}-node-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect    = "Allow"
-      Principal = { Service = "ec2.amazonaws.com" }
-      Action    = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "gitops_node_policy" {
-  role       = aws_iam_role.gitops_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "gitops_cni_policy" {
-  role       = aws_iam_role.gitops_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-}
-
-resource "aws_iam_role_policy_attachment" "gitops_registry_policy" {
-  role       = aws_iam_role.gitops_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}
-
-############################
-# EKS Cluster + Node Group
-############################
-
-resource "aws_eks_cluster" "gitops_eks" {
-  name     = "gitops-${var.environment}-eks"
-  role_arn = aws_iam_role.gitops_eks_role.arn
-  version  = "1.28"
-
-  vpc_config {
-    subnet_ids = [
-      aws_subnet.default_subnet.id,
-      aws_subnet.eks_subnet.id
-    ]
-  }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.gitops_eks_cluster_policy,
-    aws_iam_role_policy_attachment.gitops_eks_vpc_policy
-  ]
-}
-
-resource "aws_eks_node_group" "gitops_nodes" {
-  cluster_name    = aws_eks_cluster.gitops_eks.name
-  node_group_name = "default"
-  node_role_arn   = aws_iam_role.gitops_node_role.arn
-  subnet_ids      = [aws_subnet.eks_subnet.id]
-
-  scaling_config {
-    desired_size = 1
-    max_size     = 2
-    min_size     = 1
-  }
-
-  instance_types = ["t3.small"]
-  disk_size      = 30
-
-  depends_on = [
-    aws_iam_role_policy_attachment.gitops_node_policy,
-    aws_iam_role_policy_attachment.gitops_cni_policy,
-    aws_iam_role_policy_attachment.gitops_registry_policy
-  ]
-}
-
-############################
-# ECR Repository
-############################
-
-resource "aws_ecr_repository" "gitops_ecr" {
-  name = "gitops-${var.environment}-ecr"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-
-  tags = {
-    Environment = var.environment
-  }
 }
--- AWS/multi-env/iac/aws/dev/variables.tf
+++ AWS/multi-env/iac/aws/dev/variables.tf
@@ -4,15 +4,18 @@ variable "environment" {
   default     = "dev"
 }
 
-variable "location" {
-  description = "The location in which the resources will be created."
-  type        = string
-  default     = "switzerlandnorth"
+# variable "location" {
+#   description = "The location in which the resources will be created."
+#   type        = string
+#   default     = "switzerlandnorth"
 
-}
+# }
 
-variable "rg" {
-  description = "The name of the resource group in which to create the resources."
-  type        = string
-  default     = "gitops-dev-rg"
+# variable "rg" {
+#   description = "The name of the resource group in which to create the resources."
+#   type        = string
+#   default     = "gitops-dev-rg"
+# }
+variable "region" {
+  default = "us-east-1"
 }
