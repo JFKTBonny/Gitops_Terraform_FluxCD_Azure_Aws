22b697d5401ca42bff7e667a3e4b8195cadb1ab6
JFKTBonny <jkamkotoyip@yahoo.com>
2025-08-21 07:28:44 +0200

    update

--- AWS/iac/aws/vnet/main.tf
+++ AWS/iac/aws/vnet/main.tf
@@ -1,45 +1,40 @@
 
 
+provider "aws" {
+  region = "us-east-1"
+}
 
-# # AWS Provider
-# provider "aws" {
-#   region = "eu-central-2" # Switzerland (Zurich)
-# }
+# VPC (equivalent to Azure Virtual Network)
+resource "aws_vpc" "gitops_terraform_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
-# # VPC (equivalent to Virtual Network)
-# resource "aws_vpc" "gitops_terraform_vpc" {
-#   cidr_block           = "10.0.0.0/16"
-#   enable_dns_support   = true
-#   enable_dns_hostnames = true
+  tags = {
+    Name = "gitops-terraform-vpc"
+    Environment = "gitops"
+  }
+}
 
-#   tags = {
-#     Name = "gitops-terraform-vpc"
-#   }
-# }
+# Default Subnet
+resource "aws_subnet" "default_subnet" {
+  vpc_id            = aws_vpc.gitops_terraform_vpc.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "us-east-1a" # adjust to your region/AZ
 
-# # Public Subnet (equivalent to default_subnet in Azure)
-# resource "aws_subnet" "default_subnet" {
-#   vpc_id                  = aws_vpc.gitops_terraform_vpc.id
-#   cidr_block              = "10.0.0.0/24"
-#   availability_zone       = "eu-central-2a" # Pick one AZ in region
-#   map_public_ip_on_launch = true
+  tags = {
+    Name = "default-subnet"
+  }
+}
 
-#   tags = {
-#     Name = "default-subnet"
-#   }
-# }
+# Bastion Subnet
+resource "aws_subnet" "bastion_subnet" {
+  vpc_id            = aws_vpc.gitops_terraform_vpc.id
+  cidr_block        = "10.0.2.0/26"
+  availability_zone = "us-east-1b" # use a different AZ if you want HA
 
-# # Bastion Subnet (equivalent to AzureBastionSubnet)
-# resource "aws_subnet" "bastion_subnet" {
-#   vpc_id                  = aws_vpc.gitops_terraform_vpc.id
-#   cidr_block              = "10.0.2.0/26"
-#   availability_zone       = "eu-central-2b"
-#   map_public_ip_on_launch = false
-
-#   tags = {
-#     Name = "bastion-subnet"
-#   }
-# }
-
-
+  tags = {
+    Name = "bastion-subnet"
+  }
+}
 
