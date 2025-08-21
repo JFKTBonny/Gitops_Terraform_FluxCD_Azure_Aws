fb7ff12f05e9a44520f989e9c833e86b256a2f4b
JFKTBonny <jkamkotoyip@yahoo.com>
2025-08-21 06:53:05 +0200

    update

--- clusters/local/AWS/multi-env/iac/aws/base/modules/network/vpc.tf
+++
@@ -1,9 +0,0 @@
-
-resource "aws_vpc" "cluster_vpc" {
-  cidr_block = var.cidr_blocks["cluster-network"]
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-  tags = {
-    Name = "gitops-${var.environment}-vpc"
-  }
-}
